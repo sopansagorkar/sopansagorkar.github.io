f51b12248ec741a7153852bc91331056d856769f
Sopan Sagorkar <sopan.sagorkar@fisglobal.com>
2021-03-15 15:29:51 +0530

    Added Whatsapp info

---
+++ assets/js/contact.js
@@ -0,0 +1,12 @@
+function sendmsg() {
+    var name = document.querySelector("#name").value.trim();
+    var email = document.querySelector("#email").value.trim();
+    var message = document.querySelector("#message").value.trim();
+
+    const fullmsg = 'Hi Sopan, My name is *${name}*. Message: ${message}';
+
+    window.open("https://api.whatsapp.com/send?phone=+919156174248&text=" + fullmsg);
+    document.querySelector("#name").value = "";
+    document.querySelector("#email").value = "";
+    document.querySelector("#message").value = "";
+}
